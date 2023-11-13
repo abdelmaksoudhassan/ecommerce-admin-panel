ab9007ff1de048bc58bf9ea576f995b0e40a15d4
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-11-13 18:51:52 +0200

    Update PanelView.vue

--- src/views/PanelView.vue
+++ src/views/PanelView.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-aside width="55px"><SideNav></SideNav></el-aside>
-      <el-main class="margin_h padding_v"><router-view></router-view></el-main>
-    </el-container>
-  </div>
+  <el-container>
+    <el-aside width="55px"><SideNav></SideNav></el-aside>
+    <el-main class="margin_h padding_v"><router-view></router-view></el-main>
+  </el-container>
 </template>
 <script setup>
 import SideNav from '../components/side-nav.vue'
