d95e72cd11a6e00984b8a723e5dddf2f579956ab
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-12-29 23:13:31 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ app.config.globalProperties.$filters = {
 app.config.globalProperties.$axios = {
     instance(){
         return axios.create({
-            baseURL:'https://nodejs-ecommerce-o2e5.onrender.com',
+            baseURL:'https://node-js-ecommerce.vercel.app/',
             withCredentials: true
         })
     },
